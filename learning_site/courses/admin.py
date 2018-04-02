cf90b8fed4d50ebaedcc94660c24f0d88eed269f
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-04-02 02:02:35 -0700

    Added Step Mdel schema

--- learning_site/courses/admin.py
+++ learning_site/courses/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Course
+from .models import Course, Step
 
-admin.site.register(Course)
+
+class StepInline(admin.StackedInline):
+    model = Step
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [StepInline,]
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Step)
+
--- learning_site/courses/models.py
+++ learning_site/courses/models.py
@@ -9,3 +9,13 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Step(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    order = models.IntegerField(default=0)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
