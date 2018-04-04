f25d1e57b2c3b96306b64f35613c77d5c7a3e691
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-04-03 23:30:47 -0700

    Added course_detail() in courses/view.py to render course_detail.html

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Course
@@ -8,3 +7,8 @@ from .models import Course
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses': courses})
+
+
+def course_detail(request, pk):
+    course = Course.objects.get(pk=pk)
+    return render(request, 'courses/course_detail.html', {'course': coursegit})
