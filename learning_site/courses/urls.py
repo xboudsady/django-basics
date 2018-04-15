07fe7eee22c66f45d672069018b927ec7a4965dc
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-04-14 22:45:51 -0700

    Added course_detail.html and updated urls.py and views.py

--- learning_site/courses/urls.py
+++ learning_site/courses/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.course_list),
+    path('<int:course_pk>/<int:step_pk>/', views.step_detail),
     path('<int:pk>/', views.course_detail),
 ]
--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Course
+from .models import Course, Step
 
 # Create your views here.
 
@@ -12,3 +12,7 @@ def course_list(request):
 def course_detail(request, pk):
     course = get_object_or_404(Course, pk=pk)
     return render(request, 'courses/course_detail.html', {'course': course})
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'course/step_detail.html', {'step': step})
